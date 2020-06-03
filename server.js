6a6491a662cff96a1bdfe8ee7d17c6f413fc48b9
Saurabh Kumar Verma <ssaurabh.verma@hotmail.com>
2020-06-03 19:17:10 +0530

    updated

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true}) 
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true}) 
 const db = mongoose.connection
 
 db.on('error', (error) => console.error(error))
